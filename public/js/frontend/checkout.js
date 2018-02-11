19a3423f6523bfe3cd40eb15e025850685bfddaf
pramodtiwari2712@gmail.com <you@example.com>
2018-02-11 16:06:33 +0530

    procee

--- public/js/frontend/checkout.js
+++ public/js/frontend/checkout.js
@@ -1,15 +1,41 @@
-app.controller('productdetails', function ($scope, $http, $timeout) {
+//var app = angular.module('app', []);
+app.controller('chekout', function ($scope, $http, $sce, $timeout) {
     $scope.errorShow = false;
     $scope.successShow = false;
+    $scope.no_record = '';
     $scope.ajaxLoadingData = false;
-    $scope.loginData = {};
+    
+    $scope.getcartdata = function () {
+        $scope.ajaxLoadingData = true;
+        $http({
+            method: 'POST',
+            url: serverAppUrl + '/getcartdata',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        }).success(function (response) {
+            console.log(response);
+            if (response.status == 'success') {
+                $scope.productDataList = response.productDetails.data;
+                $scope.product = response.data;
+                $scope.productImage = response.imageRootPath;
+                $scope.productImageDetais = response.productDetails.productImageData;
+                $scope.ajaxLoadingData = false;
+            } else {
+                $scope.errorShow = true;
+                $scope.errorMsg = response.msg == undefined ? 'somthing went wrong ' : response.msg;
+                $timeout(function () {
+                    $scope.errorShow = false;
+                }, 2000);
+            }
+        });
+        
+    }
 
-    $scope.addtocart = function (product_id, product_name,merchant_inventry_id) {
+    $scope.addtocart = function (product_id, product_name) {
         var cartData = {};
         cartData.number_of_item = 1;
         cartData.action = 'add';
         cartData.item_name = product_name;
-        cartData.merchant_inventry_id = merchant_inventry_id;
+        cartData.merchant_inventry_id = $('#att_' + product_id).val();
         var error = ' ';
         if (cartData.item_name == undefined || cartData.item_name == '') {
             error = 'Item name should not empty';
@@ -18,7 +44,6 @@ app.controller('productdetails', function ($scope, $http, $timeout) {
             error = 'Please select attribute';
         }
         if (error == ' ') {
-            $scope.ajaxLoadingData = true;
             $http({
                 method: 'POST',
                 url: serverAppUrl + '/addtocart',
@@ -28,7 +53,7 @@ app.controller('productdetails', function ($scope, $http, $timeout) {
                 if (response.status == 'success') {
                     $scope.successShow = true;
                     $scope.successMsg = response.msg ;
-                    $scope.ajaxLoadingData = false;
+                    
                     $timeout(function(){
                         $scope.successShow = false;
                     },2000);
@@ -44,11 +69,6 @@ app.controller('productdetails', function ($scope, $http, $timeout) {
         }else{
             alert(error);
         }
-    }
-    
-    $scope.proceedtocheckout = function(){
-        var path = serverAppUrl + '/checkout';
-        window.location.href = path;
     }
 
 
