0eb9e53c4033902811e1cad5b924e7a9ab4755eb
pramodtiwari2712@gmail.com <you@example.com>
2018-02-15 23:43:20 +0530

    changes

--- public/js/frontend/product.js
+++ public/js/frontend/product.js
@@ -1,11 +1,23 @@
 //var app = angular.module('app', []);
-app.controller('product', function ($scope, $http, $sce, $timeout, categoryId) {
+app.controller('product', function ($scope, $http, $sce, $timeout, searchBy,page) {
     $scope.errorShow = false;
     $scope.successShow = false;
     $scope.no_record = '';
     $scope.searchProductParams = {};
     $scope.quantity = {};
-    $scope.searchProductParams.category_id = categoryId;
+    $scope.ajaxLoadingData = false;
+    
+    if(searchBy != undefined && searchBy != ''){
+        if(searchBy.category_id != undefined && searchBy.category_id != ''){
+            $scope.searchProductParams.category_id = searchBy.category_id;
+        }
+        
+        if(searchBy.merchant_id != undefined && searchBy.merchant_id != ''){
+            $scope.searchProductParams.merchant_id = searchBy.merchant_id;
+        } 
+    }else if(page != 'product'){
+        $scope.searchProductParams.product_type = ['offers', 'hotdeals'];
+    }
     $scope.searchProductParams.page = 1;
     $scope.allProductListResponse = {};
     var productData = {};
@@ -13,13 +25,15 @@ app.controller('product', function ($scope, $http, $sce, $timeout, categoryId) {
         $scope.no_record = 'No Data Available.';
     }
     $scope.currentPage = 1;
-    $scope.productlist = function () {        
+    $scope.productlist = function () {  
+        $scope.ajaxLoadingData = true;
         $http({
             method: 'POST',
             url: serverAppUrl + '/productlist',
             data: ObjecttoParams($scope.searchProductParams),
             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         }).success(function (response) {
+            $scope.ajaxLoadingData = false;
             if (response.status == 'success') {
                 angular.forEach(response.data, function(product, key){
                     angular.forEach(product.attribute, function(attributedata, key){
