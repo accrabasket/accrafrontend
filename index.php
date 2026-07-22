9a279b3dbebe34d3c601649f5a16affee486edad
kamal <kamalakantaetc@gmail.com>
2026-07-23 01:45:15 +0530

    ui fix

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+error_reporting(E_ERROR);
+ini_set('display_errors', 0);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 /**
  * This makes our life easier when dealing with paths. Everything is relative
